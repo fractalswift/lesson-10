726126d9a58c2648ef0d28a2c737aba9ea8fcc76
fractalswift <matthewthomasmacklin@gmail.com>
2020-07-25 15:19:09 +0100

    added cart.utils for adding multiple copy of same item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
 import { CartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
+import { addItem } from './cart.actions';
 
-const INITIAL_STATE = { hidden: true };
+const INITIAL_STATE = { hidden: true, cartItems: [] };
 
 // Every reducer gets every action, even if they are not relevant
 // That is why we have to have the default return state if not relevant action
@@ -11,6 +13,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        // The line below originally read: cartItems: [...state.cartItems, action.payload],
+
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
 
     default:
